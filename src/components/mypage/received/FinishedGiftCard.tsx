a5727b5ffa46bffe4141a6a53218034e208e3b3e
trustmitt <mainmirror14@gmail.com>
2023-02-14 02:14:31 +0900

    Docs: Update Received Gift Page 1. 펀딩 진행 중 상품 UI 구현 2. 수령한 상품 UI 구현 3. 펀딩 성공한 상품 디자인 수정 4. 버튼 클릭 시 특정 DOM으로 스크롤 이동하는 Hook 추가

---
+++ src/components/mypage/received/FinishedGiftCard.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { commaNums } from "../../../hooks/CommaNums";
+
+interface Funding {
+    fundingId: number;
+    name: string;
+    image: string;
+    price: number;
+    date: string;
+    gatherPoint: number;
+    participant: any;
+    addressInfo: boolean;
+    url: string;
+    review: string | null;
+}
+
+export default function FinishedGiftCard() {
+    const [fundingInfo, setFundingInfo] = useState<Funding[]>([]);
+    const FUNDING_URL = "/data/TestFundingData.json";
+
+    // funding data axios
+    const getFundingData = async () => {
+        await axios
+            .get(FUNDING_URL)
+            .then((res) => {
+                let response = res.data.funding;
+                setFundingInfo(response);
+            })
+            .catch((error) => {
+                return Promise.reject(error);
+            });
+    };
+
+    useEffect(() => {
+        getFundingData();
+    }, []);
+
+    return (
+        <>
+            {fundingInfo.map((gift) => (
+                <div className="Finished_Gift_Item" key={gift.fundingId}>
+                    <img className="Finished_Gift_Img" src={gift.image} alt={gift.name} />
+                    <div className="Finished_Gift_Content">
+                        <div className="Finished_Gift_Title">{gift.name}</div>
+                        <div className="Finished_Gift_Price">{commaNums(gift.price)} 원</div>
+                        <div className="Finished_Gift_Notify">이미 수령한 상품입니다.</div>
+                        <div className="Finished_Gift_Date">
+                            <div className="Finished_Gift_Date_Label">펀딩 기간</div>
+                            <div className="Finished_Gift_Date_Content">{gift.date}</div>
+                        </div>
+                        <div className="Finished_Gift_Review">
+                            <div className="Finished_Gift_Review_Label">보낸 리뷰</div>
+                            <div className="Finished_Gift_Review_Content">{gift.review ? gift.review : "리뷰를 작성하지 않았습니다."}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
