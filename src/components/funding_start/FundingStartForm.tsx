550bed89162761886d19ffb78cff682374c3aebe
hyeJune <chj950807@naver.com>
2023-02-19 01:37:19 +0900

    FEAT: FriendPicker email add

--- src/components/funding_start/FundingStartForm.tsx
+++ src/components/funding_start/FundingStartForm.tsx
@@ -31,7 +31,7 @@ interface FriendsObj {
 }
 
 export default function FundingStartForm() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [items, setItems] = useState<ProductObj | null>(null);
   const [friends, setFriends] = useState<FriendsObj[]>([]);
 
@@ -104,139 +104,134 @@ export default function FundingStartForm() {
     e.preventDefault();
     //1.날짜현재시간이후 , 2.친구선택, 3.금액이 1000원 이상
     if (endDate === null) {
-        setErrorCode(1)
-        setErrorShow(true);
+      setErrorCode(1);
+      setErrorShow(true);
     } else if (pickFriend === null) {
-        setErrorCode(2)
-        setErrorShow(true);
+      setErrorCode(2);
+      setErrorShow(true);
     } else if (fundingAmout < 1000) {
-        setErrorCode(3)
-        setErrorShow(true);
+      setErrorCode(3);
+      setErrorShow(true);
     } else {
-        setErrorCode(0)
-        setErrorShow(true);
-        console.log("product: " +id);
-        console.log(new Date());
-        console.log(endDate);
-        console.log(pickFriend);
-        console.log(fundingAmout);
-        navigate('/');
+      setErrorCode(0);
+      setErrorShow(true);
+      console.log("product: " + id);
+      console.log(new Date());
+      console.log(endDate);
+      console.log(pickFriend);
+      console.log(fundingAmout);
+      navigate("/");
     }
   };
 
   const handleClose = (e: React.MouseEvent<HTMLButtonElement> | void) => {
     setErrorShow(false);
   };
+
   useEffect(() => {
     getItems();
-  }, []);
-
-  useEffect(() => {
     getFriends();
-  }, []);
-
-  useEffect(() => {}, [fundingAmout]);
+  }, [items, friends]);
 
   return (
     <>
       <div className="FundingStart_Container">
         <div className="FundingStart_Wrapper">
           {items != null ? (
-            <>
-              <form onSubmit={onSubmitHandler} target="blankifr">
-                <div className="FundingStart_Top_Area">
-                  <div>
-                    <img
-                      src={items.image}
-                      alt={items.name}
-                      className="FundingStart_Img"
-                    />
+            <form onSubmit={onSubmitHandler} target="blankifr">
+              <div className="FundingStart_Top_Area">
+                <div>
+                  <img
+                    src={items.image}
+                    alt={items.name}
+                    className="FundingStart_Img"
+                  />
+                </div>
+                <div className="FundingStart_Desc">
+                  <div className="FundingStart_Title">{items.name}</div>
+                  <div className="FundingStart_Price">
+                    {commaNums(items.price)} 원
                   </div>
-                  <div className="FundingStart_Desc">
-                    <div className="FundingStart_Title">{items.name}</div>
-                    <div className="FundingStart_Price">
-                      {commaNums(items.price)} 원
-                    </div>
-                    <hr />
-                    <div className="FundingStart_DatePicker">
-                      <h2>펀딩 기간 선택</h2>
-                      <div>
-                        <DatePicker
-                          placeholderText="펀딩 기간을 선택해 주세요"
-                          selected={endDate}
-                          onChange={(date) => setEndDate(date)}
-                          showTimeSelect
-                          dateFormat="yyyy년 MM월 dd일 a hh시"
-                          timeFormat="HH:mm"
-                          timeIntervals={60}
-                          filterTime={fileterPassedTime}
-                          locale={ko}
-                          minDate={new Date()}
-                        />
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="FundingStart_FriendPicker">
-                      <h2>펀딩 받을 친구 선택</h2>
-                      <ul className="FriendPicker_Wrapper">
-                        {friends.map((data: FriendsObj) => (
-                          <FriendPicker
-                            key={data.Userid}
-                            Userid={data.Userid}
-                            name={data.name}
-                            profile={data.profile}
-                            FriendSelect={(Userid, name, profile) =>
-                              onFriendsSelectHandler(Userid, name, profile)
-                            }
-                          />
-                        ))}
-                      </ul>
-                      <div className="Selected_Wrapper">
-                        <span>
-                          <h2>선택된 친구</h2>
-                        </span>
-                        <div>
-                          {pickFriendProfile ? (
-                            <div className="Selected_Friend">
-                              <div className="Friend_desc">
-                                <img
-                                  className="Friend_image"
-                                  src={pickFriendProfile}
-                                  alt="friend-image"
-                                />
-                                <span>{pickFriendName}</span>
-                              </div>
-                              <button
-                                className="btn btn-primary"
-                                type="button"
-                                onClick={onCancelSelect}
-                              >
-                                취소
-                              </button>
-                            </div>
-                          ) : (
-                            <div>선택된 친구가 없습니다.</div>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="FundingStart_Desc_Bottom">
-                      <label htmlFor="Funding_Amount">내 펀딩 금액:</label>
-                      <input
-                        id="Funding_Amount"
-                        type="number"
-                        onChange={onChangeAmount}
+                  <hr />
+                  <div className="FundingStart_DatePicker">
+                    <h2>펀딩 기간 선택</h2>
+                    <div>
+                      <DatePicker
+                        placeholderText="펀딩 기간을 선택해 주세요"
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        showTimeSelect
+                        dateFormat="yyyy년 MM월 dd일 a hh시"
+                        timeFormat="HH:mm"
+                        timeIntervals={60}
+                        filterTime={fileterPassedTime}
+                        locale={ko}
+                        minDate={new Date()}
                       />
-
-                      <button type="submit" className="btn btn-warning">
-                        펀딩 시작
-                      </button>
                     </div>
+                  </div>
+                  <hr />
+                  <div className="FundingStart_FriendPicker">
+                    <h2>펀딩 받을 친구 선택</h2>
+                    <ul className="FriendPicker_Wrapper">
+                      {friends.map((data: FriendsObj) => (
+                        <FriendPicker
+                          key={data.Userid}
+                          Userid={data.Userid}
+                          name={data.name}
+                          profile={data.profile}
+                          email={data.email}
+                          FriendSelect={(Userid, name, profile) =>
+                            onFriendsSelectHandler(Userid, name, profile)
+                          }
+                        />
+                      ))}
+                    </ul>
+                    <div className="Selected_Wrapper">
+                      <span>
+                        <h2>선택된 친구</h2>
+                      </span>
+                      <div>
+                        {pickFriendProfile ? (
+                          <div className="Selected_Friend">
+                            <div className="Friend_desc">
+                              <img
+                                className="Friend_image"
+                                src={pickFriendProfile}
+                                alt="friend-image"
+                              />
+                              <span>{pickFriendName}</span>
+                            </div>
+                            <button
+                              className="btn btn-primary"
+                              type="button"
+                              onClick={onCancelSelect}
+                            >
+                              취소
+                            </button>
+                          </div>
+                        ) : (
+                          <div>선택된 친구가 없습니다.</div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="FundingStart_Desc_Bottom">
+                    <label htmlFor="Funding_Amount">내 펀딩 금액:</label>
+                    <input
+                      id="Funding_Amount"
+                      type="number"
+                      onChange={onChangeAmount}
+                    />
+
+                    <button type="submit" className="btn btn-warning">
+                      펀딩 시작
+                    </button>
                   </div>
                 </div>
-              </form>
-            </>
+              </div>
+            </form>
           ) : null}
         </div>
       </div>
