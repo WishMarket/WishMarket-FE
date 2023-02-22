f06b07560697ac901f64b719afed2968b17717fb
조혜준 <112573111+chj950807@users.noreply.github.com>
2023-02-22 23:44:05 +0900

    FEAT: Login getTokens (#41)
    * FIX: Funding-addition UI bug Fix
    
    * FEAT: FriendPicker email add
    
    * REFACTOR: FundingStatForm 코드분할
    
    * REFACTOR: Funding Addition 코드분할
    
    * backup
    
    * FIX: FundingAdditionDate add
    
    * REFACTOR: Friends UI& 구조 변경
    
    * FEAT: FriendWishlist add
    
    * backup2
    
    * FEAT: Login getToken

---
+++ src/components/friends/FamousFriendForm.tsx
@@ -0,0 +1,47 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
+import FamousFriendCard from "./card/FamousFriendCard";
+
+interface FamousFriendsObj {
+  Userid: string;
+  name: string;
+  nickname: string;
+  profileImage: string;
+  famous: boolean;
+  isfriend: boolean;
+}
+
+
+export default function FamousFriendForm() {
+  const [famouseusers, setFamousUsers] = useState<FamousFriendsObj[]>([]);
+  
+    const url2 = "/data/FamousFriends.json";
+    const getFamousUsers = async () => {
+      await axios
+        .get(url2)
+        .then((res) => {
+          let response = res.data.famousfriend;
+          setFamousUsers(response); // 연동 시 교체 필요
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        });
+    };
+
+  useEffect(() => {
+     getFamousUsers();
+  }, [])
+  
+  return (
+    <div className="FamousFriend">
+      <div className="FamousFriend_title">
+        🔥 추천하는&nbsp;<h2>인플루언서</h2>&nbsp; 목록입니다!
+      </div>
+      <div className="FriendResult_List">
+        {famouseusers.map((data: FamousFriendsObj) => {
+          return <FamousFriendCard data={data} key={data.Userid} />;
+        })}
+      </div>
+    </div>
+  );
+}
