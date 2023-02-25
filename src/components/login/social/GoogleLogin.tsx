8350f633d3f92943b358a889e0817fdc88c1782f
hyeJune <chj950807@naver.com>
2023-02-26 01:29:27 +0900

    backup

--- src/components/login/social/GoogleLogin.tsx
+++ src/components/login/social/GoogleLogin.tsx
@@ -3,11 +3,11 @@ import { FcGoogle } from "react-icons/fc";
 
 export default function GoogleLogin() {
   const Google_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
-  const google_URL: string = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${Google_KEY}&response_type=token&redirect_uri=http://localhost:5173/login/google/redirect&scope=https://www.googleapis.com/auth/userinfo.email`;
+  const google_URL: string = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${Google_KEY}&response_type=token&redirect_uri=http://localhost:5173/api/auth/sign-in/google`;
 
   return (
     <div>
-      <a href={google_URL} className="Link_Wrapper">
+      <a href={google_URL} className="Link_Wrapper" target="_blank">
         <button
           className="login_Button_Google"
           type="button"
