a61c57f495824e7e69a6081fea17b65589f1a4cd
조혜준 <112573111+chj950807@users.noreply.github.com>
2023-03-05 22:08:37 +0900

    FIX: FriendSearch Error fix (#64)
    * REFACTOR: Funding Addition 코드분할
    
    * backup
    
    * FIX: FundingAdditionDate add
    
    * REFACTOR: Friends UI& 구조 변경
    
    * FEAT: FriendWishlist add
    
    * backup2
    
    * FEAT: Login getToken
    
    * FEAT: Sign-up API update
    
    * FEAT: FindPasswd & ChangePasswd API uptate
    
    * backup
    
    * FEAT: Logout & Withdrawal API Change
    
    * CHORE : .gitignore change
    
    * Chore: env Remove
    
    * REFACTOR: Signup Refactor
    
    * REFACTOR: Login Refactor
    
    * backup
    
    * backup2
    
    * backup
    
    * FEAT: SearchFriend API Change
    
    * FEAT: FundingStart API 적용
    
    * FEAT:Funding 참여 API 추가
    
    * backup
    
    * backup
    
    * FEAT: backup
    
    * REFACTOR: 토큰 함수 구조 변경 및 Link설정
    
    * backup
    
    * FIX: AccessToken 만료시 친구찾기 오류 수정
    
    * Revert "FIX: AccessToken 만료시 친구찾기 오류 수정"
    
    This reverts commit 0c37989dbd8a5900aae7287eafc64567b88fb521.
    
    * FIX: FriendSearch AccessToken 만료시 생기는 오류FIX
    
    * backup

--- src/hooks/axios/SearchFriend.ts
+++ src/hooks/axios/SearchFriend.ts
@@ -60,19 +60,17 @@ export const FriendsFollowDelete = async (followId: number) => {
 };
 
 export const InfluencerFriend = async () => {
-  const Influencer_URL = "http://3.38.63.3:8080/api/follow/influence";
-  return await axios
-    .get(Influencer_URL, {
-      headers: {
-        Authorization: window.localStorage.getItem("accessToken"),
-      },
-      withCredentials: true,
-    })
+  const Influencer_URL = "http://3.38.63.3:8080/api/follow/influencer";
+  return await axios.get(Influencer_URL, {
+    headers: {
+      Authorization: window.localStorage.getItem("accessToken"),
+    },
+    withCredentials: true,
+  })
     .then((res) => {
       let response = res.data;
       return response;
-    })
-    .catch((error) => {
+    }).catch((error) => {
       return error;
-    });
-};
+  })
+}
