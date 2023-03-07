91029866f536ea6bb1afb7aecdf2ac1f1cdecc31
mitt <111720709+trustmitt@users.noreply.github.com>
2023-03-07 11:28:10 +0900

    Fix: 펀딩 시작 페이지 프로필 처리 (#70)
    * backup #001
    
    * Feat: Add UI for categories page
    1. Create subcategory tab for category page
    2. Update dummy json data
    3. Funding slide bug fix
    
    * Feat: Add UI for categories page
    >> 1. Create subcategory tab for category page
    >> 2. Update dummy json data
    >> 3. Funding slide bug fix
    >> 4. Badge for best Item only
    
    * Refactor: Using axios package
    1. Axios 적용
    2. 카테고리 정렬 수정
    
    * Refactor: Using axios package
    1. Axios 적용
    2. 카테고리 정렬 수정
    
    * Docs: Item category fix
    
    * backup #02
    
    * Docs: Update main css for conflict
    
    * Docs: sub category path fix
    1. 서브 카테고리 경로 수정
    2. 기존에 있던 Matters 영역 삭제 처리
    
    * backup #03
    
    * Docs: Add validation for modify profile page
    1. 프로필 페이지 유효성 검사 기능 추가
    2. 리뷰 컴포넌트 분리
    3. 헤더 로고 사이즈 수정
    
    * backup #04
    
    * Feat: Implements wishlist UI
    
    * Docs: Update Received Gift Page
    1. 펀딩 진행 중 상품 UI 구현
    2. 수령한 상품 UI 구현
    3. 펀딩 성공한 상품 디자인 수정
    4. 버튼 클릭 시 특정 DOM으로 스크롤 이동하는 Hook 추가
    
    * Feat: Funding Account Page
    1. 펀딩 내역 페이지 UI
    2. 스크롤 버튼 이벤트 select로 변경
    
    * gitignore test #01
    
    * Rename: components 하위에 마이 페이지 폴더 분리
    1. components 폴더와 pages 폴더의 가시성 위해 폴더 분리
    2. changepasswd 파일명 수정
    3. 캐러셀 이미지 교체
    4. 로고 교체
    5. 파비콘 교체
    
    * backup #06
    
    * Design: 회원가입, 비밀번호 변경 디자인 수정
    1. 회원가입 UI 디자인 변경
    2. 비밀번호 찾기, 변경 UI 디자인 수정
    3. footer 수정
    
    * backup #08
    
    * Design: 펀딩 시작, 친구 찾기 디자인 수정
    1. 펀딩 시작 페이지 UI 변경
    2. Input 태그 step 추가
    3. 친구 찾기 UI 변경
    
    * Docs: 친구 목록 페이지 분리
    1. 친구 목록 페이지 분리 및 디자인 수정
    
    * Docs: 친구 목록 페이지 수정
    1. 친구 목록 컴포넌트 position 속성 변경
    2. overflow 삭제 및 스크롤 이벤트 추가
    3. 세부 디자인 변경
    
    * Docs: 페이지 권한 설정
    1. 로컬 스토리지 accessToken 값으로 페이지 권한 설정
    2. 로그아웃 기능 추가
    3. 헤더 컴포넌트 리팩토링
    
    * backup #08
    
    * Docs: Axios Best Category 페이지 변수 추가
    
    * backup #09
    
    * Refactor: 코드 리팩토링
    1. 상품 검색 키워드 없는 경우 버튼 hidden 처리
    2. 코드 리팩토링
    3. interface 파일 분리
    
    * Docs: Post Wishlist 연동
    
    * Docs: 위시리스트 기능 연동
    
    * backup #10
    
    * fix: 친구 목록 수정
    
    * Docs: 받은 선물 조건부 렌더링 추가
    1. 받은 선물 조건부 렌더링 추가
    2. 리뷰 코멘트 공백일 경우 처리
    
    * Feat: 친구 펀딩 내역 연동
    1. 친구 펀딩 목록 없을 경우 오류 발생
    
    * Feat: 알림 기능 조건부 렌더링 추가
    
    * Feat: 프로필 수정 연동
    
    * Fix: 펀딩 시작 페이지 프로필 처리

--- src/components/funding_start/FriendPicker.tsx
+++ src/components/funding_start/FriendPicker.tsx
@@ -1,44 +1,27 @@
 import React from "react";
+import defaultImg from "../../assets/default-profile-img.png";
 import { FriendPickerProps } from "./FundingStart.interface";
 
-export default function FriendPicker({
-  email,
-  userId,
-  name,
-  profileImageUrl,
-  FriendSelect,
-}: FriendPickerProps) {
-  const clickSelect = (
-    userId: number,
-    name: string,
-    profileImageUrl: string
-  ) => {
-    FriendSelect(userId, name, profileImageUrl);
-  };
+export default function FriendPicker({ email, userId, name, profileImageUrl, FriendSelect }: FriendPickerProps) {
+    const clickSelect = (userId: number, name: string, profileImageUrl: string) => {
+        FriendSelect(userId, name, profileImageUrl);
+    };
 
-  return (
-    <>
-      <li className="Friend">
-        <div className="Friend_desc">
-          <img
-            className="Friend_image"
-            src={profileImageUrl}
-            alt="friend-image"
-          />
-          <div className="Friend_info">
-            <span>{name}</span>
-            <span className="Friend_email">{email}</span>
-          </div>
-        </div>
+    return (
+        <>
+            <li className="Friend">
+                <div className="Friend_desc">
+                    <img className="Friend_image" src={profileImageUrl ? profileImageUrl : defaultImg} alt="friend-image" />
+                    <div className="Friend_info">
+                        <span>{name}</span>
+                        <span className="Friend_email">{email}</span>
+                    </div>
+                </div>
 
-        <button
-          className="btn btn-primary"
-          onClick={() => clickSelect(userId, name, profileImageUrl)}
-          type="button"
-        >
-          선택
-        </button>
-      </li>
-    </>
-  );
+                <button className="btn btn-primary" onClick={() => clickSelect(userId, name, profileImageUrl)} type="button">
+                    선택
+                </button>
+            </li>
+        </>
+    );
 }
